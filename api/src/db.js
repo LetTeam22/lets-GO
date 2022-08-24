c68f040d395e17bdde9345e8f7a98a743670442d
FelipeJure <felipe.jure05@gmail.com>
2022-08-24 11:52:07 -0300

    limpie Back

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,10 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame, Genre } = sequelize.models;
+const { } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Videogame.belongsToMany(Genre,{through:'videogames_genres'});
-Genre.belongsToMany(Videogame,{through:'videogames_genres'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,11 @@
 const { Router } = require('express');
-const videogamesRouter = require('./videogamesRouter')
-const videogameRouter = require('./videogameRouter')
-const genreRouter = require('./genreRouter')
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
-router.use('/videogames', videogamesRouter)
-router.use('/videogame', videogameRouter)
-router.use('/genres', genreRouter)
+// router.use('/', nombre )
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
