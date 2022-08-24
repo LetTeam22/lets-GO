352bb360f61ba84465436725beb5f7f2b85b3c66
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-08-24 14:47:16 -0300

    Acomode las rutas de importacion

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import { Route } from "react-router-dom";
-import LandingPage from "./Components/LandingPage";
-import Home from "./Components/Home";
-import Profile from "./Components/Profile";
-import BikeDetail from "./Components/BikeDetail";
-import Navbar from "./Components/Navbar";
+import LandingPage from "./Components/LandingPage/LandingPage.jsx";
+import Home from "./Components/Home/Home";
+import Profile from "./Components/Profile/Profile";
+import BikeDetail from "./Components/BikeDetail/BikeDetail";
+import Navbar from "./Components/NavBar/Navbar.jsx";
 import "./App.css";
 
 function App() {
