6e7e804dec214764b8414cc64b722627f7eedd73
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-08-25 12:01:13 -0300

    acomode detalles del home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -5,12 +5,15 @@ import { GoLocation } from 'react-icons/go';
 
 export default function Home () {
     return (
-        <div className={s.home}>  
-            <div className={s.location}>
-                <h3 className={s.title}>San Miguel de Tucuman, Argentina</h3>
-                <GoLocation color='#BEB9B9' size='26px' />
-            </div>
+        <>
             <NavBar />
-        </div>
+            <div className={s.home}>  
+                <div className={s.location}>
+                    <h3 className={s.title}>San Miguel de Tucuman, Argentina</h3>
+                    <GoLocation color='#BEB9B9' size='26px' />
+                </div>
+            </div>
+        </>
+        
     )
 }
