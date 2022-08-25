3543aa01c86775ddf7d6c79990e8d06b799139a2
FelipeJure <felipe.jure05@gmail.com>
2022-08-25 12:24:51 -0300

    navbar sticky

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import { Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Profile from "./Components/Profile/Profile";
 import BikeDetail from "./Components/BikeDetail/BikeDetail";
+import NavBar from './Components/NavBar/Navbar';
+
 import "./App.css";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Route exact path="/">
         <Home />
       </Route>
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import NavBar from '../NavBar/Navbar';
 import s from './Home.module.css';
 import { GoLocation } from 'react-icons/go';
 
 export default function Home () {
     return (
-        <>
-            <NavBar />
             <div className={s.home}>  
                 <div className={s.location}>
                     <h3 className={s.title}>San Miguel de Tucuman, Argentina</h3>
                     <GoLocation color='#BEB9B9' size='26px' />
                 </div>
-            </div>
-        </>
-        
+            </div>    
     )
 }
