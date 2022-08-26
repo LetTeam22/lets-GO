863c4bbea0ce17380a761e990db6c4e2ae9916de
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-08-26 14:43:57 -0300

    Le agregue un link para que desde bicicletas podamos acceder al Home

--- client/src/Components/Menu/Menu.jsx
+++ client/src/Components/Menu/Menu.jsx
@@ -5,13 +5,17 @@ import carrito from "../../image/carrito.png";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogIn from "../NavBar/Authentication/LogIn";
 import LogOut from "../NavBar/Authentication/LogOut";
+import { Link } from "react-router-dom";
+
 
 export const Menu = () => {
   const { isAuthenticated } = useAuth0();
   return (
     <div className={s.menu}>
       <div className={s.options}>
-        <span>BICICLETAS</span>
+        <Link to='/home'>
+          <span>BICICLETAS</span>
+        </Link>
         <hr color="#F9B621" />
       </div>
       <div className={s.options}>
