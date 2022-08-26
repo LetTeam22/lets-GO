458016f731bfbd39af6bc1cee2528775e6c2ae5a
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-08-26 18:05:29 -0300

    Componente experiencies

---
+++ client/src/Components/Experiencies/Experiencies.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './/Experiencies.module.css';
+import experiencies from '../../image/experiencias1.png'
+
+const Experiencies = () => {
+
+    return (
+        <div className={s.experiencies}>
+            <span className={s.title}>EXPERIENCIAS</span>
+            <p className={s.text}>Conoce las experiencias de otras personas y comparti tu aventura con bicis let's GO</p>
+            <h1>0% emisión 100% emoción</h1>
+            <img src={experiencies} alt="experiencies" className={s.imgExperiencies} />
+        </div>
+    )
+}
+
+export default Experiencies;
