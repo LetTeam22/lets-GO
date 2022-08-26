c2c50882e3204b1354ea3aa5a885fa6f0b58117e
mrtovar10 <mrtovar10@gmail.com>
2022-08-26 18:28:48 -0400

    modelo usuario

--- api/src/controllers/userControllers.js
+++ api/src/controllers/userControllers.js
@@ -6,10 +6,16 @@ const {User, Bike, Booking} = require ('../db.js')
 async function getAllUsers(req, res, next) {
    let users = await User.findAll();
     // console.log(users[0].toJSON())
+    res.send(users)
 }
 
 
 
+// crea un usuario
+async function createUser(req, res, next) {
+    const {userEmail} = req.query
+    
+}
 
 module.exports = {getAllUsers}
 
