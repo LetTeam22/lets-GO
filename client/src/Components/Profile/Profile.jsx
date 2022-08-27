f8c01529391f85d2e946f4e92be2ecd8138a08bc
FelipeJure <felipe.jure05@gmail.com>
2022-08-27 13:25:49 -0300

    creacion de logica

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
 import {
@@ -14,12 +14,48 @@ import theme from "./MaterialUIColors";
 import s from "./Profile.module.css";
 import { ThemeProvider } from "@emotion/react";
 
+const validate = (input, id) => {
+  let errors = {}
+  console.log(id)
+  if(id === 'firstName' && !/^[A-Z]+$/i.test(input.firstName)) errors.firstName = 'Solo se aceptan letras'
+  if(id === 'lastName' && !/^[A-Z]+$/i.test(input.lastName)) errors.lastName = 'Solo se aceptan letras'
+  if(id === 'cellphone' && !/^[0-9]\d{9}$/.test(input.cellphone)) errors.cellphone = 'Solo se aceptan numeros'
+  return errors
+}
+
 export const Profile = () => {
   const { user, isLoading } = useAuth0();
   const history = useHistory();
-  console.log(user);
+  const [input, setInput] = useState({
+    firstName:'',
+    lastName:'',
+    cellphone:''
+  })
+  // const [desabled, setDesabled] = useState(true)
+  const [errors, setErrors] = useState({})
+  // console.log(user);
   if (isLoading) return <Loading/>
   
+  const handleChange = e => {
+    setInput({
+      ...input,
+      [e.target.id]: e.target.value
+    })
+    setErrors(validate({
+      ...input,
+      [e.target.id]: e.target.value
+    }, e.target.id))
+  }
+  const handleSubmit = e => {
+    e.preventDefault()
+    setInput({
+      firstName:'',
+      lastName:'',
+      cellphone:''
+    })
+  }
+  // console.log(input)
+  const disabled = Object.keys(errors).length > 0 || !input.firstName || !input.lastName || !input.cellphone
   return (
     <>
       <div className={s.container}>
@@ -27,25 +63,27 @@ export const Profile = () => {
           <h2>{user.name}</h2>
           <img src={user.picture} alt={user.name} className={s.img} />
         </div>
-        <form className={s.form}>
+        <form className={s.form} onSubmit={handleSubmit}>
           <ThemeProvider theme={theme}>
             <FormControl>
               <InputLabel htmlFor="firstName">Nombre</InputLabel>
-              <Input id="my-input" aria-describedby="my-helper-text" />
+              <Input id="firstName" aria-describedby="my-helper-text" error={errors.firstName?true:false} value={input.firstName} onChange={handleChange} required/>
             </FormControl>
             <FormControl>
               <InputLabel htmlFor="lastName">Apellido</InputLabel>
-              <Input id="lastName" aria-describedby="my-helper-text" />
+              <Input id="lastName" aria-describedby="my-helper-text" error={errors.lastName?true:false} value={input.lastName} onChange={handleChange} required/>
               <FormHelperText id="my-helper-text"></FormHelperText>
             </FormControl>
             <FormControl>
               <InputLabel htmlFor="cellphone">Telefono</InputLabel>
-              <Input id="cellphone" aria-describedby="my-helper-text" />
+              <Input id="cellphone" aria-describedby="my-helper-text" error={errors.cellphone?true:false} type="tel" placeholder="0111234567" value={input.cellphone} onChange={handleChange} required/>
             </FormControl>
             <Button
               variant="contained"
               endIcon={<IoSend />}
               className={s.btnSend}
+              type='submit'
+              disabled={disabled}
             >
               Send
             </Button>
@@ -57,6 +95,7 @@ export const Profile = () => {
         color="success"
         className={s.btnHome}
         onClick={() => history.push("/")}
+        
       >
         Go Home
       </Button>
