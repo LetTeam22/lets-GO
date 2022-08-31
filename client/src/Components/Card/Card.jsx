6181d130c5079f2219b11deff4b8ca2af4d729a1
Nico <nicolasbalbi90@gmail.com>
2022-08-31 17:31:22 -0300

    retoques varios a la home (filtros, card)

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,18 +1,57 @@
 import React from 'react';
 import s from './Card.module.css';
+import imgRat0 from '../../image/stars/0stars.png'
+import imgRat05 from '../../image/stars/0.5star.png'
+import imgRat1 from '../../image/stars/1star.png'
+import imgRat15 from '../../image/stars/1.5stars.png'
+import imgRat2 from '../../image/stars/2stars.png'
+import imgRat25 from '../../image/stars/2.5stars.png'
+import imgRat3 from '../../image/stars/3stars.png'
+import imgRat35 from '../../image/stars/3.5stars.png'
+import imgRat4 from '../../image/stars/4stars.png'
+import imgRat45 from '../../image/stars/4.5stars.png'
+import imgRat5 from '../../image/stars/5stars.png'
+import rodado from '../../image/rueda_bici.png'
+import mecanica from '../../image/mecanica.png'
+import electrica from '../../image/electrica.png'
 
 
 export const Card = ({ name, type, image, traction, wheelSize, price, rating, id }) => {
+
+    const imgRating = rat => {
+        if (rat < 0.5) return imgRat0
+        if (rat < 1.5) return imgRat05
+        if (rat < 2.5) return imgRat1
+        if (rat < 3.5) return imgRat15
+        if (rat < 4.5) return imgRat2
+        if (rat < 5.5) return imgRat25
+        if (rat < 6.5) return imgRat3
+        if (rat < 7.5) return imgRat35
+        if (rat < 8.5) return imgRat4
+        if (rat < 9.5) return imgRat45
+        return imgRat5
+    } 
 
     return (
         <div className={id % 2 === 0 ? `${s.card}` : `${s.cardTwo}`}>
             <img src={image} alt='img not found' className={s.imgCard} /> 
             <div>
-                <h3 className={s.name}>{name}</h3>              
-                <h4 className={s.data}>{type} </h4>
-                <h4 className={s.data}>Tracción: {traction}</h4>
-                <h4 className={s.data}>Rodado: {wheelSize}</h4>
-                <h4 className={s.data}>Raiting: {rating} ☆</h4>
+                <h3 className={s.name}>{name}</h3>
+                <div className={s.dataCont}>
+                    <span className={s.type}>{type} </span>
+                    {/* <h4 className={s.data}>Tracción: {traction}</h4> */}
+                    <img className={traction === 'eléctrica' ? s.electrica : s.mecanica} src={traction === 'eléctrica' ? electrica : mecanica} alt='Tracción '/>
+                    {/* <h4 className={s.data}>Rodado: {wheelSize}</h4> */}
+                    <div className={s.rodadoCont}>
+                        <img className={s.rueda} src={rodado} alt='Rodado '/>
+                        <span className={s.rodado}>{wheelSize}</span>
+                    </div>
+                </div>              
+                {/* <h4 className={s.data}>Rating: {rating} ☆</h4> */}
+                <div className={s.ratingCont}>
+                    <img className={s.stars} src={imgRating(rating)} alt='Rating '/>
+                    <span className={s.rating}>{rating}</span>
+                </div>
                 <h4 className={s.price}>${price}/día</h4>
             </div>
                               
--- client/src/Components/Orderings/Orderings.jsx
+++ client/src/Components/Orderings/Orderings.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { setCurrentPage, setParameters } from "../../Redux/actions";
+import { useSelector } from "react-redux";
 import s from './Orderings.module.css';
-// import { FiltersSelected } from "../FiltersSelected/FiltersSelected";
 
 const Orderings = ({handleParameter}) => {
+
+    const parameters = useSelector(state => state.parameters);
 
     const handlePriceSort = e => {
         handleParameter(e, 'price', e.target.value, 'Precio', 'priceSort', 'sorts')
@@ -22,21 +22,21 @@ const Orderings = ({handleParameter}) => {
         <>
             <div className={s.container} >
                 <span className={s.spanOrderings}>Ordenar por precio:</span>
-                <select value='price' onChange={handlePriceSort} className={s.orderings} id='priceSort'>
+                <select value='price' onChange={handlePriceSort} className={parameters.sorts.price ? `${s.orderings} ${s.act}` : s.orderings} id='priceSort'>
                     <option value=''></option>
                     <option value='asc'>menor precio</option>
                     <option value='desc'>mayor precio</option>
                 </select>
 
                 <span className={s.spanOrderings}>Ordenar por Rating:</span>
-                <select value='rating' onChange={handleRatingSort} className={s.orderings} id='ratingSort'>
+                <select value='rating' onChange={handleRatingSort} className={parameters.sorts.rating ? `${s.orderings} ${s.act}` : s.orderings} id='ratingSort'>
                     <option value=''></option>
                     <option value='asc'>menor rating</option>
                     <option value='desc'>mayor rating</option>
                 </select>
 
                 <span className={s.spanOrderings}>Ordenar por nombre:</span>
-                <select value='name' onChange={handleNameSort} className={s.orderings} id='nameSort'>
+                <select value='name' onChange={handleNameSort} className={parameters.sorts.name ? `${s.orderings} ${s.act}` : s.orderings} id='nameSort'>
                     <option value=''></option>
                     <option value='asc'>A-Z</option>
                     <option value='desc'>Z-A</option>
