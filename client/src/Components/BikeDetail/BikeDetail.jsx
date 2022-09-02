b2f5df91eb9dd58d4eb6dff9644a0c59edb133b3
FelipeJure <felipe.jure05@gmail.com>
2022-09-02 08:47:33 -0300

    saque el localStorage del carrito

--- client/src/Components/BikeDetail/BikeDetail.jsx
+++ client/src/Components/BikeDetail/BikeDetail.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom'
 import swal from 'sweetalert';
 import { getBikeDetail, addBooking, getAccesories } from '../../Redux/actions';
+import Loading from '../Loading/Loading';
 import s from './BikeDetail.module.css';
 import icon from '../../image/bicisDestacadas/icon.png';
 import tech from '../../image/Technology.png';
