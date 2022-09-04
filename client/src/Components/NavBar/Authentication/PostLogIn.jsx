0c10236e7e0812f4e84ad8bacae6d88ac9d5d558
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-09-04 01:20:45 -0300

    acomode algo de las variables de entorno que me habia olvidado

--- client/src/Components/NavBar/Authentication/PostLogIn.jsx
+++ client/src/Components/NavBar/Authentication/PostLogIn.jsx
@@ -8,7 +8,7 @@ import postlogin from '../../../image/postlogin.png';
 import emailjs from '@emailjs/browser';
 
 const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-const TEMPLATE_ID = 'template_luhi41q';
+const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID2;
 const PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
 
 export default function PostLogIn () {
