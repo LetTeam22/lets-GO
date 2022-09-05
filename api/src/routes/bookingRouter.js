622ed75dc204a9d80833fba734d3ea595232bb51
Nico <nicolasbalbi90@gmail.com>
2022-09-05 01:43:00 -0300

    ruta get bookings by bike id

--- api/src/routes/bookingRouter.js
+++ api/src/routes/bookingRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { getAllBookings, getBookingsByUserId, postBooking, cancelBooking } = require('../controllers/bookingsControllers');
+const { getAllBookings, getBookingsByUserId, getBookingsByBikeId, postBooking, cancelBooking } = require('../controllers/bookingsControllers');
 
 const router = Router();
 router.get('/', getAllBookings)
+router.get('/bike/:bikeId', getBookingsByBikeId)
 router.get('/:userId', getBookingsByUserId)
 router.post('/', postBooking)
 router.put('/:bookingId', cancelBooking)
