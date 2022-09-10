3296dddf9fa87c8f4a3e8b571a3d5c1f96175bbc
FelipeJure <felipe.jure05@gmail.com>
2022-09-09 22:34:09 -0300

    responsive Landing

--- client/src/Components/Menu/Menu.jsx
+++ client/src/Components/Menu/Menu.jsx
@@ -6,36 +6,68 @@ import s from "./Menu.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogIn from "../NavBar/Authentication/LogIn";
 import LogOut from "../NavBar/Authentication/LogOut";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 // import logo from "../../image/logo.png";
-
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import { TbDiscount2, TbMessageDots } from 'react-icons/tb'
 
 export const Menu = () => {
   const logo = "https://res.cloudinary.com/pflet/image/upload/v1662686136/Let/image/logo_vwis1a.png"
+  const adventure = 'https://res.cloudinary.com/pflet/image/upload/v1662748275/Let/image/adv_hyo69c.png'
+  const experience = 'https://res.cloudinary.com/pflet/image/upload/v1662742235/Let/image/exp_clwf94.png'
   const { isAuthenticated } = useAuth0();
+  const location = useLocation()
+
+  const url = location.pathname
 
   return (
-    <div className={s.menu}>
-      <Link to="/"><img src={logo} alt="logo" className={s.icon} /></Link>
+    <div className={s.menu} >
+      <Link to="/"><img src={logo} alt="logo" className={s.icon}/></Link>
       <div className={s.options}>
-        <div >
-          <Link to='/home'><span className={s.span}>BICICLETAS</span></Link>
+        <div>
+          <Link to='/home'>
+            <div className={url === '/home'? s.active : null}>
+              <DirectionsBikeIcon className={s.responsiveIcons}/>
+            </div>
+            <span className={s.span}>BICICLETAS</span>
+            </Link>
         </div>
         <div >
-          <Link to='/adventure'><span className={s.span}>AVENTURAS</span></Link>
+          <Link to='/adventure'>
+          <div className={url === '/adventure'? `${s.active} ${s.img}` : s.img}>
+              <img src={adventure} className={s.responsiveIcons} alt='aventura'/>
+            </div>
+            <span className={s.span}>AVENTURAS</span>
+          </Link>
         </div>
         <div >
-          <Link to='/promotions'><span className={s.span}>BENEFICIOS</span></Link>
+          <Link to='/promotions'>
+            <div className={url === '/promotions'? s.active : null}>
+              <TbDiscount2 className={s.responsiveIcons}/>
+            </div>
+            <span className={s.span}>BENEFICIOS</span>
+          </Link>
         </div>
         <div >
-          <Link to='/allExperiencies'><span className={s.span}>EXPERIENCIAS</span></Link>
+          <Link to='/allExperiencies'>
+          <div className={url === '/allExperiencies'?  `${s.active} ${s.img}` : s.img}>
+              <img src={experience} className={s.responsiveIcons} alt='experiencia'/>
+            </div>
+            <span className={s.span}>EXPERIENCIAS</span>
+          </Link>
         </div>
         <div >
-          <Link to='/contact'><span className={s.span}>CONTACTO</span></Link>
+          <Link to='/contact'>
+            <div className={url === '/contact'? s.active : null}>
+              <TbMessageDots className={s.responsiveIcons}/>
+            </div>
+            <span className={s.span}>CONTACTO</span>
+          </Link>
         </div>
       </div>
-
-      <SearchBar />
+      <div className={s.searchbarContainer}>
+        <SearchBar />
+      </div>
       <div className={s.login}>
         {isAuthenticated ? <LogOut /> : <LogIn />}
         <Link to='/cart'>
