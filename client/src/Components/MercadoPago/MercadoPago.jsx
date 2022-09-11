c2ee38066eb297ffe5df8504f6e189fe28918dc6
Lucia Meyer <luciameyer1603@gmail.com>
2022-09-10 21:58:01 -0300

    silencio mercadopago

--- client/src/Components/MercadoPago/MercadoPago.jsx
+++ client/src/Components/MercadoPago/MercadoPago.jsx
@@ -1,40 +1,40 @@
-import React, { useEffect } from 'react'
-import { useMercadopago } from 'react-sdk-mercadopago';
+// import React, { useEffect } from 'react'
+// import { useMercadopago } from 'react-sdk-mercadopago';
 
-const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY_MP;
+// const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY_MP;
 
-const Mp = ( {preference, mpInfo} ) => {
+// const Mp = ( {preference, mpInfo} ) => {
 
-    const mercadopago = useMercadopago.v2(PUBLIC_KEY, {
-        locale: 'es-AR'
-    });
-
-
-    useEffect(() => {
-
-        const container = document.querySelector('.cho-container');
-        const btn = document.querySelector('.mercadopago-button');
-
-        if (mercadopago) {
-            if(btn)  {
-                container.removeChild(btn)
-            }
-            mercadopago.checkout({
-                preference: {
-                    id: mpInfo.id
-                },
-                render: {
-                    container: '.cho-container',
-                    label: 'Pagar',
-                }
-            })
-        }
-    }, [mercadopago, mpInfo]) // eslint-disable-line react-hooks/exhaustive-deps
-
-    return (
-        <div className="cho-container" />
-    )
-}
+//     const mercadopago = useMercadopago.v2(PUBLIC_KEY, {
+//         locale: 'es-AR'
+//     });
 
 
-export default Mp;
+//     useEffect(() => {
+
+//         const container = document.querySelector('.cho-container');
+//         const btn = document.querySelector('.mercadopago-button');
+
+//         if (mercadopago) {
+//             if(btn)  {
+//                 container.removeChild(btn)
+//             }
+//             mercadopago.checkout({
+//                 preference: {
+//                     id: mpInfo.id
+//                 },
+//                 render: {
+//                     container: '.cho-container',
+//                     label: 'Pagar',
+//                 }
+//             })
+//         }
+//     }, [mercadopago, mpInfo]) // eslint-disable-line react-hooks/exhaustive-deps
+
+//     return (
+//         <div className="cho-container" />
+//     )
+// }
+
+
+// export default Mp;
