0beb73ca150cc175197e07b9aea3f670b23abc82
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-09-12 20:15:57 -0300

    Agregue boton para poner me gusta en las experiencias

--- client/src/Components/CardsExperiences/CardExperiences.jsx
+++ client/src/Components/CardsExperiences/CardExperiences.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './CardExperiences.module.css'
-
+import { BiLike } from 'react-icons/bi';
+import { AiFillLike } from 'react-icons/ai';
 
 
 
 
 const CardExperience = ({ imgExperience, textExperience, firstName}) => {
+
+    const [ like, setLike ] = useState(false);
+    
+    const handleLike = (e) => {
+        e.preventDefault();
+        setLike(!like);
+    }
 
     return (
         <div className={s.blogCard} >
@@ -14,6 +22,11 @@ const CardExperience = ({ imgExperience, textExperience, firstName}) => {
                 <h2 className={s.h2}>Los leters andan diciendo...</h2>
                 <h1 className={s.h1}>{firstName}</h1>
                 <p className={s.p}>{textExperience}</p>
+                {
+                    like 
+                    ? <button onClick={e => handleLike(e)} className={s.iconBtn}><AiFillLike size='1.5rem' color='#F9B621' /></button>
+                    : <button onClick={e => handleLike(e)} className={s.iconBtn}><BiLike size='1.5rem' color='#F9B621'/></button>
+                }
             </div>
         </div >
     )
