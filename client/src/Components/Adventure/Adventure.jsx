e874523928b9e37f8cf24500b57a8cf2648fdbff
Insólita <soledato@gmail.com>
2022-09-12 02:06:21 -0300

    agrego aventuras al componente existente

--- client/src/Components/Adventure/Adventure.jsx
+++ client/src/Components/Adventure/Adventure.jsx
@@ -1,14 +1,36 @@
 import React, { useEffect } from 'react'
 import s from './Adventure.module.css';
+import adventures from './data';
+import CardAdventures from './CardAdventures';
 // import aux_aventuras from '../../image/aux_/aux_aventuras.png';
 
 export const Adventure = () => {
-    
+
+console.log(adventures)
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
 
     return (
-        <img src="https://res.cloudinary.com/pflet/image/upload/v1662686120/Let/image/aux_/aux_aventuras_ir3wap.png" alt='aux_aventuras' className={s.img_aux} />
+        // <img src="https://res.cloudinary.com/pflet/image/upload/v1662686120/Let/image/aux_/aux_aventuras_ir3wap.png" alt='aux_aventuras' className={s.img_aux} />
+        <div>
+
+            <div className={s.container}>
+                <h1 className={s.h1}>Exclusivo para leters aventureros</h1>
+            </div>
+
+            {
+                adventures.map(a => <CardAdventures
+                    name={a.name}
+                    date={a.date}
+                    image={a.image}
+                    description={a.description}
+                    conditions={a.conditions}
+                    price={a.price}
+                    difficulty={a.difficulty}
+                    />)
+                }
+                
+        </div>
     )
 };
---
+++ client/src/Components/Adventure/CardAdventures.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './CardAdventures.module.css'
+
+
+
+
+
+const CardAdventures = ({ name, description, date, conditions, image, price, difficulty }) => {
+    console.log(name, date)
+
+    return (
+        <div className={s.blogCard} >
+            <img className={s.imgcard} src={image} alt='img not found' /> 
+            <div className={s.description}>
+                <h2 className={s.h2}>{date} / Dificultad {difficulty}</h2>
+                <h2 className={s.h1}>{name}</h2>
+                <p className={s.p}>{description}</p>
+                <p className={s.p2}>{conditions}</p>
+                <p className={s.p}>Precio: ${price}</p>
+            {/* <button className={s.btn2}><img style={{width:"50%"}} src="https://res.cloudinary.com/pflet/image/upload/v1662686105/Let/image/carrito_wohy11.png" alt="img not found"/></button> */}
+            </div>
+        </div >
+    )
+};
+
+export default CardAdventures
