cdcd8aca448fd13812e4de824faab8f6e6912d25
Juan Martin Silva <juanmartinsilva74@gmail.com>
2022-09-18 13:36:34 -0300

    cambios en transiciones, y dinamismo en las experiencias landing

--- client/src/Components/Experiencies/Experiencies.jsx
+++ client/src/Components/Experiencies/Experiencies.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import s from './Experiencies.module.css';
-// import experiencies from '../../image/experiencias1.png';
+
 
 export const Experiencies = () => {
-    const experiencies = 'https://res.cloudinary.com/pflet/image/upload/v1662730961/Let/image/experiencias1_nraf3v.png'
+    
+    const allExperiences = useSelector((state) => state.allExperiences);
+
+    console.log(allExperiences);
+
     return (
         <div className={s.experiencies}>
             <span className={s.title}>EXPERIENCIAS</span>
             <p className={s.text}>Conocé las experiencias de otras personas y compartí tu aventura con bicis let's GO</p>
             <h1 className={s.h1}>0% emisión 100% emoción</h1>
-            <img src={experiencies} alt="experiencies" className={s.imgExperiencies} />
+            <div className={s.experiencesImg}>
+                {
+                    allExperiences?.slice(0, 5).map(experience => {
+                        return (
+                            <div className={s.img}>
+                                <img src={experience.imgExperience} alt="img" className={s.experienceImg} />
+                                <div className={s.hoverDiv}>
+                                    <h3>{experience.firstName}</h3>
+                                    <span>{experience.booking.endDate}</span>
+                                    <p>{experience.textExperience}</p>
+                                </div>
+                            </div>    
+                        )
+                    })
+                }
+                <div className={s.experiencesBtn}>
+                    <Link to='/allExperiencies'>
+                        <div className={s.btn}>VER TODAS LAS EXPERIENCIAS</div>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 };
