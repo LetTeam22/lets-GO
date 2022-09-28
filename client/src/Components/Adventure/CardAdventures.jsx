41a5166ea04335b053475a69a744cc6c32ca5cb3
FelipeJure <felipe.jure05@gmail.com>
2022-09-28 10:53:47 -0300

    responsive aventuras y corazon de Card

--- client/src/Components/Adventure/CardAdventures.jsx
+++ client/src/Components/Adventure/CardAdventures.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
-import s from './CardAdventures.module.css'
+import React from "react";
+import s from "./CardAdventures.module.css";
 
-
-
-
-const CardAdventures = ({ id, handleCheck, name, description, date, conditions, image, price, difficulty }) => {
-
-    return (
-        <div className={s.blogCard} >
-            <img className={s.imgcard} src={image} alt='img not found' />
-            <div className={s.description}>
-                <h2 className={s.h2}>{date} / Dificultad {difficulty}</h2>
-                <h2 className={s.h1}>{name}</h2>
-                <p className={s.p}>{description}</p>
-                <p className={s.p2}>{conditions}</p>
-                <div className={s.boxes} >
-                    <p className={s.price}>Precio: ${price}</p>
-                    <input
-                        id={id}
-                        type="checkbox"
-                        name={name}
-                        onClick={handleCheck}
-                        className={s.input}
-                    />
-                    <label className={s.label} htmlFor={id}>Me subo a esta aventura</label>
-                </div>
-
-            </div>
-        </div >
-    )
+const CardAdventures = ({
+  id,
+  handleCheck,
+  name,
+  description,
+  date,
+  conditions,
+  image,
+  price,
+  difficulty,
+}) => {
+  return (
+    <div className={s.blogCard}>
+      <h2 className={s.h2}>
+        {date} / Dificultad {difficulty}
+      </h2>
+      <h2 className={s.h1}>{name}</h2>
+      <section className={s.container}>
+        <div>
+          <img className={s.imgcard} src={image} alt="img not found" />
+        </div>
+        <div className={s.description}>
+          <div className={s.descriptionDiv}>
+            <p className={s.p}>{description}</p>
+          </div>
+          <div className={s.descriptionDiv}>
+            <p className={s.p2}>{conditions}</p>
+          </div>
+          <div className={s.boxes}>
+            <p className={s.price}>Precio: ${price}</p>
+            <input
+              id={id}
+              type="checkbox"
+              name={name}
+              onClick={handleCheck}
+              className={s.input}
+            />
+            <label className={s.label} htmlFor={id}>
+              Me subo a esta aventura
+            </label>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
-export default CardAdventures
+export default CardAdventures;
