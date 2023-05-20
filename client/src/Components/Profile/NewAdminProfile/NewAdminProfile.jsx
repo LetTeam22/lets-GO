3c44fe7bda8f91996813c5f1010b4c688b23f12d
FelipeJure <felipe.jure05@gmail.com>
2023-05-20 21:33:16 +0200

    fix buttons in newAdminProfile

--- client/src/Components/Profile/NewAdminProfile/NewAdminProfile.jsx
+++ client/src/Components/Profile/NewAdminProfile/NewAdminProfile.jsx
@@ -1,69 +1,42 @@
 import React, { useState } from "react";
 import s from "./NewAdminProfile.module.css";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { Button } from "./Button/Button";
 import { dashboardItems } from "./DashboardItems";
 import Bookings from "../AdminProfile/Bookings";
-import RenderProfilePic from '../../Cloudinary/renderProfilePic';
-
+import { AdminData } from "./AdminData/AdminData";
 
 export const NewAdminProfile = () => {
-
-  const image = "https://res.cloudinary.com/pflet/image/upload/v1662686111/Let/image/persona_logeada_hatkhk.png"
-  const userLogged = useSelector(state => state.user);
   const [showTable, setShowTable] = useState(<Bookings />);
   const [active, setActive] = useState("Reservas");
-
 
   const handleClick = (table, value) => {
     setShowTable(table);
     setActive(value);
   };
 
-  const showedName = (userLogged.firstName && userLogged.lastName)
-    ? `${userLogged.firstName} ${userLogged.lastName}`
-    : userLogged.firstName ? userLogged.firstName : userLogged?.email
-
   return (
     <section className={s.panel}>
-
       <div className={s.lateral}>
-
-        <div className={s.containerUs}>
-            <div className={s.containerImg}>
-              {userLogged?.profilePic ?
-                <Link to='/editProfile'>
-                  <RenderProfilePic publicId={userLogged.profilePic} alt={userLogged?.name} />
-                </Link>
-                :
-                <Link to='/editProfile'>
-                  <img img className={s.img} src={image} alt={userLogged?.firstName || null} />
-                </Link>
-              }
-            </div>
-            <h4 className={s.data}>{showedName}</h4>
-            <div className={s.line}></div>
+        <AdminData />
+        <div className={s.buttons}>
+          {dashboardItems.map(({ value, table, childrens }) => {
+            return (
+              <Button
+                onClick={handleClick}
+                active={active}
+                value={value}
+                table={table}
+                childrens={childrens}
+                key={value}
+              >
+                {value}
+              </Button>
+            );
+          })}
         </div>
-
-        {dashboardItems.map(({ value, table, childrens }) => {
-          return (
-            <Button
-              onClick={handleClick}
-              active={active}
-              value={value}
-              table={table}
-              childrens={childrens}
-              key={value}
-            >
-              {value}
-            </Button>
-          );
-        })}
       </div>
 
       <aside className={s.tables}>{showTable}</aside>
-
     </section>
   );
 };
--- client/src/Components/Profile/NewAdminProfile/SubItems/Subitems.jsx
+++ client/src/Components/Profile/NewAdminProfile/SubItems/Subitems.jsx
@@ -7,28 +7,28 @@ export const SubItems = ({ children, onClick, active, childrens, value }) => {
   const [show, setShow] = useState(false);
 
   return (
-    <div className={s.containerBtn}>
-      <div className={s.btnAndBox}>
-        <Button onClick={() => setShow(!show)} active={active} value={value}>
-          {children}
-          <Box show={show}/>
-        </Button>
-      </div>
-
-      {show &&
-        childrens.map(({ value, table, childrens }) => {
-          return (
-            <Button
-              onClick={() => onClick(table, value)}
-              active={active}
-              value={value}
-              table={table}
-              childrens={childrens}
-            >
-              {value}
-            </Button>
-          );
-        })}
+    <div className={`${s.containerBtn} ${show ? s.active : ""}`}>
+      <Button onClick={() => setShow(!show)} active={active} value={value}>
+        {children}
+        <Box show={show} onClick={() => setShow(!show)} />
+      </Button>
+      {show && (
+        <section>
+          {childrens.map(({ value, table, childrens }) => {
+            return (
+              <Button
+                onClick={() => onClick(table, value)}
+                active={active}
+                value={value}
+                table={table}
+                childrens={childrens}
+              >
+                {value}
+              </Button>
+            );
+          })}
+        </section>
+      )}
     </div>
   );
 };
